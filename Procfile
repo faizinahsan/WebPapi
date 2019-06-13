d24086b20428feb11d205be6df1bf0351a179122
faizinahsan <faizin110998@gmail.com>
2019-06-14 05:34:30 +0700

    Ready to deploy v1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WebPapi.wsgi--log-file -
--- WebPapi/settings.py
+++ WebPapi/settings.py
@@ -120,8 +120,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 AUTH_USER_MODEL = 'users.User'
 
 LOGIN_REDIRECT_URL ='index'
